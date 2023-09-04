3ff61dddaa841f93bbd70e3cb5d226a2e2cd6e04
Hwangyongjin <dswvgw6412@naver.com>
2023-09-04 10:08:54 +0900

    feat: modify Detail

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Navigation from "../components/Navigation";
+import "../style/Detail.css";
 
 const Detail = () => {
   const location = useLocation();
@@ -17,7 +18,23 @@ const Detail = () => {
   return (
     <div>
       <Navigation />
-      {title}
+      <div id="detailParentDiv">
+        <img src={poster} alt={title} title={title} />
+        <div id="detailData">
+          <h3 id="detailTitle">{title}</h3>
+          <h5 id="detailYear">{year}</h5>
+          <ul id="detailGenres">
+            {genres.map((genre, i) => {
+              return (
+                <li id="detailGenre" key={i}>
+                  {genre}
+                </li>
+              );
+            })}
+          </ul>
+          <p id="detailSummary">{summary}...</p>
+        </div>
+      </div>
     </div>
   );
 };
